d39c28a5663482d25e5afd0e58bfcd5efeea6546
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-15 14:08:47 -0400

    deleted code we don't need & added in components

---
+++ src/components/Results.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Results() {
+    return (
+      <div>
+        <h1>IMAGE, NAME, PHONE, EMAIL, etc</h1>
+      </div>
+    );
+  }
+  
+  export default Results
