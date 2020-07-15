4f9b92b9c3008868477c50d3cfbc910857aa3197
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-15 17:12:08 -0400

    column headers now in and JSON file created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./components/Header";
 import Search from "./components/SearchForm";
+import Results from "./components/Results";
 
 function App() {
   return (
       <div>
         <Header />  
         <Search />  
+        <Results />
       </div>
   );
 }
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,32 @@
 import React from "react";
 import "../index.css";
 
-function Results() {
-    return (
-      <div>
-        <h1>IMAGE, NAME, PHONE, EMAIL, etc</h1>
-      </div>
-    );
-  }
+
+function Results(props) {
+  return (
+    <div className="container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              <strong id="col">First Name</strong> {props.first_name}
+            </th>
+            <th>
+              <strong id="col">Last Name</strong> {props.last_name}
+            </th>
+            <th>
+              <strong id="col">Job Title</strong> {props.job_title}
+            </th>
+            <th>
+              <strong id="col">Email Address</strong> {props.email}
+            </th>
+          </tr>
+        </thead>
+      </table>
+      <span onClick={() => props.removeFriend(props.id)} className="remove">
+      </span>
+    </div>
+  );
+}
   
-  export default Results
+  export default Results;
