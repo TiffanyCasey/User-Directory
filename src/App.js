cd364012d53d8f329b0a86b93cba7c65c1b0a032
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-23 18:46:51 -0400

    removed json file from app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Results from "./components/Results";
-import employees from "./employees.json";
 
 class App extends Component {
-
-  state = {
-    employees, 
-    searchTerm: ''
-  };
-
   render() {
     return (
       <div>
